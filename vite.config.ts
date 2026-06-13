f94d2c19f9c512a0556cccf1924a1732de9b20ed
Ammar sheikh <ammarsaboor40@gmail.com>
2026-06-13 13:18:46 -0400

    Change dev server port to 5174 to avoid conflict with other local projects

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,11 @@ const base = isGithubPages ? '/Brainstorm/' : './';
 export default defineConfig(() => {
   return {
     server: {
-      port: 3000,
+      port: 5174,
       host: '0.0.0.0',
     },
     preview: {
-      port: 3000,
+      port: 5174,
       host: '0.0.0.0',
     },
     base,
